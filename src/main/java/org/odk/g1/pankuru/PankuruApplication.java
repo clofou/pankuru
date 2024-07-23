00de33b09983127cdea7963a17f19523bcf8e15d
Fakoro <fakoro88@gmail.com>
2024-07-23 10:59:54 +0000

    first commit

---
+++ src/main/java/org/odk/g1/pankuru/PankuruApplication.java
@@ -0,0 +1,13 @@
+package org.odk.g1.pankuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PankuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PankuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/odk/g1/pankuru/PankuruApplicationTests.java
@@ -0,0 +1,13 @@
+package org.odk.g1.pankuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PankuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
