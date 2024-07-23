a1a7c718f24dbece38892c65ad6a784d5b7099ee
Fakoro <fakoro88@gmail.com>
2024-07-23 15:38:40 +0000

    creation des repo du package Humain

---
+++ src/main/java/org/odk/g1/pankuru/Repository/HumainRepo/AdresseRepo.java
@@ -0,0 +1,8 @@
+package org.odk.g1.pankuru.Repository.HumainRepo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdresseRepo extends JpaRepository<AdresseRepo, Long> {
+}
