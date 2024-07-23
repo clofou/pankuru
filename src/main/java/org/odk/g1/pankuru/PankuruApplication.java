ca4e4dc4b44cc3115960dc020a1a6dbbf6a7d0f9
Fakoro <fakoro88@gmail.com>
2024-07-23 11:29:57 +0000

    desactivation de spring security

--- src/main/java/org/odk/g1/pankuru/PankuruApplication.java
+++ src/main/java/org/odk/g1/pankuru/PankuruApplication.java
@@ -2,8 +2,9 @@ package org.odk.g1.pankuru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PankuruApplication {
 
     public static void main(String[] args) {
